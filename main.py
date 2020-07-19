73cfaa5fac6b5c90b85a4b0411e699c498e71c6d
Chad Gavin Sia Si Cong <csia5929@uni.sydney.edu.au>
2020-07-19 15:27:28 +0800

    added isolation forest for anomoly detection

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from matplotlib import gridspec
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import IsolationForest
+from sklearn.metrics import confusion_matrix
 
 def visualiseFeatures(data):
     print(data.shape)
@@ -25,7 +26,7 @@ def determineFraud(data):
     print('Valid Transactions: {}'.format(len(data[data['Class'] ==0])))
     print("Amount details of fraudulent transaction")
     
-    return Fraud
+    return Fraud , outlier_fraction
 
 def heatmap(data):
     corrmat = data.corr()
@@ -42,7 +43,27 @@ def divideToFeaturesNTarget(data):
     X_data=X.values
     Y_data=Y.values
     X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size = 0.2, random_state = 42)
+    return  X_train, X_test, Y_train, Y_test
 
+def isolationForest( X_train, X_test, Y_train, Y_test,outlier_fraction):
+    ifc=IsolationForest(max_samples=len(X_train),
+    contamination=outlier_fraction,random_state=1)
+    ifc.fit(X_train)
+    scores_pred = ifc.decision_function(X_train)
+    y_pred = ifc.predict(X_test)
+
+    y_pred[y_pred == 1] = 0     #build Evaluation Matrix on Test Set
+    y_pred[y_pred == -1] = 1
+    n_errors = (y_pred != Y_test).sum()
+
+    LABELS = ['Normal', 'Fraud']
+    conf_matrix = confusion_matrix(Y_test, y_pred)
+    plt.figure(figsize=(12, 12))
+    sns.heatmap(conf_matrix, xticklabels=LABELS,yticklabels=LABELS, annot=True, fmt='d')
+    plt.title('Confusion matrix')
+    plt.ylabel('True class')
+    plt.xlabel('Predicted class')
+    plt.show()
 def main():
     data = pd.read_csv('creditcard.csv')
     features, gs = visualiseFeatures(data)
@@ -54,10 +75,11 @@ def main():
     #     ax.set_title('histogram of feature: ' + str(c))
     # plt.show()
 
-    Fraud=determineFraud(data)
+    Fraud, outlier_fraction = determineFraud(data)
     print(Fraud.Amount.describe())
     heatmap(data)
-
+    X_train, X_test, Y_train, Y_test = divideToFeaturesNTarget(data)
+    isolationForest(X_train, X_test, Y_train, Y_test,outlier_fraction)
     
 
 if __name__ == "__main__":
